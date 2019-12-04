3d72fccc6f770e6660c56ec3ee4e781f47ea099a
Julie Nguyen <julie-nguyen@live.com>
2019-12-04 18:34:49 +0000

    Initial commit of client
    modularized the client side

---
+++ client.js
@@ -0,0 +1,19 @@
+const net = require('net');
+
+
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: 'localhost',
+    port: 50541
+  });
+  
+  conn.on('data', (data) => {
+    console.log(data);
+  });
+  
+  conn.setEncoding('utf8'); 
+
+  return conn;
+}
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,23 +1,5 @@
-const net = require('net');
 
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-  });
-  conn.on('data', (data) => {
-    console.log(data);
-  });
-
-  // interpret incoming data as text
-
-  conn.setEncoding('utf8'); 
-
-  return conn;
-}
-
+// play.js
+const connect = require('./client');
 console.log('Connecting ...');
 connect();
