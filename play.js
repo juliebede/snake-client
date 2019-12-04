e53082acbd7616c84e95edc4349d5de176c4b873
Julie Nguyen <julie-nguyen@live.com>
2019-12-04 19:30:45 +0000

    Set up to handle user input

--- play.js
+++ play.js
@@ -2,4 +2,20 @@
 // play.js
 const connect = require('./client');
 console.log('Connecting ...');
-connect();
+
+const handlerUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+}
+
+const setupInput = function(data) {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handlerUserInput);
+  return stdin;
+}
+
+setupInput(handlerUserInput);
