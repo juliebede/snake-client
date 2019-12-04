b7fb5a0d22aa63c391f11cd40fd276fba0f64b00
Julie Nguyen <julie-nguyen@live.com>
2019-12-04 18:51:35 +0000

    added connection message and name display

--- client.js
+++ client.js
@@ -6,7 +6,13 @@ const connect = function() {
     host: 'localhost',
     port: 50541
   });
+
+  conn.on('connect', () => {
+    console.log('Sucessfully connected to game server');
+  });
   
+  conn.write('Name: JNG')
+
   conn.on('data', (data) => {
     console.log(data);
   });
